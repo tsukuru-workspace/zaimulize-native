38b2715ab7e38fe386d3d08670affcb21509165e
Masahiro Hori <good.bye.baseball.17@gmail.com>
2017-11-26 15:30:46 +0900

    使用していない変数を削除

--- zaimulize/App.js
+++ zaimulize/App.js
@@ -1,29 +1,12 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
  * @flow
  */
-
-import React, { Component } from 'react';
+import React from 'react';
 import { TabNavigator } from 'react-navigation';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {
-    Platform,
-    StyleSheet,
-    Text,
-    View,
-} from 'react-native';
 import HomeScreen from './src/components/homeScreen/index';
 import TrendScreen from './src/components/trendScreen/index';
-import SearchHistroyScreen from './src/components/searchHistoryScreen/index';
+import SearchHistoryScreen from './src/components/searchHistoryScreen/index';
 import TimelyNewsScreen from './src/components/timelyNewsScreen/index';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 const RootTabs = TabNavigator({
    Home: {
@@ -39,7 +22,7 @@ const RootTabs = TabNavigator({
         },
     },
     SearchHistory: {
-        screen: SearchHistroyScreen,
+        screen: SearchHistoryScreen,
         navigationOptions: {
             tabBarLabel: '検索履歴',
         },
